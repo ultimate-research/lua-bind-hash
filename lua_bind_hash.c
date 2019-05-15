82bb4469a49d4113e594ddda566ac1a782a08745
jugeeya <jugeeya@live.com>
2019-05-15 15:17:09 -0700

    reimplement C impl, slightly change naming scheme

--- lua_bind_hash.c
+++ lua_bind_hash.c
@@ -5,78 +5,91 @@
 
 uint64_t lua_bind_hash(void* data_, size_t len) {
     int64_t *data = data_;
-    int64_t *plVar1;
-    int64_t *plVar2;
-    uint64_t uVar3;
-    uint64_t uVar4;
-    int64_t lVar5;
-    uint64_t uVar6;
-    uint64_t uVar7;
-    uint64_t uVar8;
-    uint64_t uVar9;
-    uint64_t uVar10;
-    uint64_t uVar11;
+    size_t hash = len;
+    int64_t hash_add = 0x27d4eb2f165667c4;
+    int64_t* data_end = (int64_t *)((int64_t)data + len);
+
+    uint64_t hash_vals[4] = {
+       0x60ea27eeadc0b5d5,
+       0xc2b2ae3d27d4eb4e,
+       0xffffffffffffffff,
+       0x61c8864e7a143578,
+    };
+
+    uint64_t hash_vals_mid[4];
+
+    const uint64_t hash_vals_end[4] = {
+        0x3c6ef3630bd7950e,
+        0x1bbcd8c2f5e54380,
+        0x779b185ebca87000,
+        0xe6c617af2a1c0000
+    };
+
+    const uint64_t hash_vals_end_2[4] = {
+        0x3f,
+        0x39,
+        0x34,
+        0x2e
+    };
+
+    const uint64_t MULT1 = 0x87bcb65480000000; // -0x784349ab80000000
+    const uint64_t MULT2 = 0xdef35b010f796ca9; // -0x210ca4fef0869357
+    const uint64_t MULT3 = 0x9e3779b185ebca87; // -0x61c8864e7a143579
+    const uint64_t MULT4 = 0xc2b2ae3d27d4eb4f; // -0x3d4d51c2d82b14b1
+    const uint64_t MULT5 = 0x93ea75a780000000; // -0x6c158a5880000000
+    const uint64_t MULT6 = 0x27d4eb2f165667c5;
+    const uint64_t ADD1 = 0x85ebca77c2b2ae63;
+    const uint64_t ADD2 = 0x165667b19e3779f9;
     
-    lVar5 = 0x27d4eb2f165667c4;
-    plVar1 = (int64_t *)((int64_t)data + len);
-    if (0x1f < len) {
-        uVar9 = 0xc2b2ae3d27d4eb4e;
-        uVar6 = 0x61c8864e7a143578;
-        uVar7 = 0xffffffffffffffff;
-        uVar3 = 0x60ea27eeadc0b5d5;
+    if (len >= 32) {
         do {
-            uVar3 = uVar3 + *data * -0x3d4d51c2d82b14b1;
-            uVar4 = uVar3 >> 0x21 | uVar3 * 0x80000000;
-            uVar9 = uVar9 + data[1] * -0x3d4d51c2d82b14b1;
-            uVar7 = uVar7 + data[2] * -0x3d4d51c2d82b14b1;
-            uVar6 = uVar6 + data[3] * -0x3d4d51c2d82b14b1;
-            uVar10 = uVar9 >> 0x21 | uVar9 * 0x80000000;
-            uVar9 = uVar10 * -0x61c8864e7a143579;
-            uVar11 = uVar7 >> 0x21 | uVar7 * 0x80000000;
-            uVar8 = uVar6 >> 0x21 | uVar6 * 0x80000000;
-            uVar3 = uVar4 * -0x61c8864e7a143579;
-            uVar7 = uVar11 * -0x61c8864e7a143579;
-            uVar6 = uVar8 * -0x61c8864e7a143579;
-            data = data + 4;
-        } while (data <= plVar1 + -4);
-        lVar5 = (((((uVar10 * 0x1bbcd8c2f5e54380 | uVar9 >> 0x39) +
-                    (uVar4 * 0x3c6ef3630bd7950e | uVar3 >> 0x3f) +
-                    (uVar11 * 0x779b185ebca87000 | uVar7 >> 0x34) +
-                    (uVar8 * -0x1939e850d5e40000 | uVar6 >> 0x2e) ^
-                    (uVar4 * -0x784349ab80000000 | uVar4 * -0x210ca4fef0869357 >> 0x21) *
-                    -0x61c8864e7a143579) * -0x61c8864e7a143579 + 0x85ebca77c2b2ae63 ^
-                    (uVar10 * -0x784349ab80000000 | uVar10 * -0x210ca4fef0869357 >> 0x21) *
-                    -0x61c8864e7a143579) * -0x61c8864e7a143579 + 0x85ebca77c2b2ae63 ^
-                    (uVar11 * -0x784349ab80000000 | uVar11 * -0x210ca4fef0869357 >> 0x21) *
-                    -0x61c8864e7a143579) * -0x61c8864e7a143579 + 0x85ebca77c2b2ae63 ^
-                    (uVar8 * -0x784349ab80000000 | uVar8 * -0x210ca4fef0869357 >> 0x21) *
-                    -0x61c8864e7a143579) * -0x61c8864e7a143579 + -0x7a1435883d4d519d;
+          for (int i = 0; i < 4; i++) {
+            hash_vals[i] += data[i] * MULT4;
+            hash_vals_mid[i] = hash_vals[i] >> 0x21 | hash_vals[i] * 0x80000000;
+            hash_vals[i] = hash_vals_mid[i] * MULT3;
+          }
+          
+          data = data + 4;
+        } while (data <= data_end + -4);
+        uint64_t val = 0;
+        for (int i = 0; i < 4; i++) {
+          val += (hash_vals_mid[i] * hash_vals_end[i] | hash_vals[i] >> hash_vals_end_2[i]);
+        }
+
+        val = (val ^ (hash_vals_mid[0] * MULT1 | hash_vals_mid[0] * MULT2 >> 0x21) * MULT3) * MULT3;
+        for (int i = 1; i < 4; i++) {
+          val = (val + ADD1 ^ (hash_vals_mid[i] * MULT1 | hash_vals_mid[i] * MULT2 >> 0x21) * MULT3) * MULT3;
+        }
+        val += ADD1;
+
+        hash_add = val;
     }
-    plVar2 = data + 1;
-    len = lVar5 + len;
-    while (plVar2 <= plVar1) {
-        plVar2 = data + 2;
-        len = (*data * -0x6c158a5880000000 | (uint64_t)(*data * -0x3d4d51c2d82b14b1) >> 0x21) *
-                    -0x61c8864e7a143579 ^ len;
-        len = (len >> 0x25 | len << 0x1b) * -0x61c8864e7a143579 + 0x85ebca77c2b2ae63;
-        data = data + 1;
+
+    hash += hash_add;
+    for (; data + 1 <= data_end; data++) {
+      hash = (*data * MULT5 | (*data * MULT4) >> 0x21) *
+                    MULT3 ^ hash;
+      hash = (hash >> 0x25 | hash << 0x1b) * MULT3 + ADD1;
     }
-    if ((int64_t *)((int64_t)data + 4U) <= plVar1) {
-        len = (uint64_t)*(uint32_t *)data * -0x61c8864e7a143579 ^ len;
-        len = (len >> 0x29 | len << 0x17) * -0x3d4d51c2d82b14b1 + 0x165667b19e3779f9;
-        data = (int64_t *)((int64_t)data + 4U);
+
+    int32_t* data_32 = (int32_t*) data; 
+    if ((int64_t *)(data_32 + 1) <= data_end) {
+        hash = (uint64_t)*data_32 * MULT3 ^ hash;
+        hash = (hash >> 0x29 | hash << 0x17) * MULT4 + ADD2;
+        data = (int64_t *)(data_32 + 1);
     }
-    if (data < plVar1) {
-        do {
-            plVar2 = (int64_t *)((int64_t)data + 1);
-            len = (uint64_t)*(uint8_t *)data * 0x27d4eb2f165667c5 ^ len;
-            len = (len >> 0x35 | len << 0xb) * -0x61c8864e7a143579;
-            data = plVar2;
-        } while (plVar1 != plVar2);
+
+    int8_t* data_8 = (int8_t*) data;
+
+    for (; data != data_end; data = (int64_t*)data_8) {
+      hash = (uint64_t)(*data_8) * MULT6 ^ hash;
+      hash = (hash >> 0x35 | hash << 0xb) * MULT3;
+      data_8++;
     }
-    uVar3 = (len ^ len >> 0x21) * -0x3d4d51c2d82b14b1;
-    uVar3 = (uVar3 ^ uVar3 >> 0x1d) * 0x165667b19e3779f9;
-    return uVar3 ^ uVar3 >> 0x20;
+    
+    uint64_t final_hash = (hash ^ hash >> 0x21) * MULT4;
+    final_hash = (final_hash ^ final_hash >> 0x1d) * ADD2;
+    return final_hash ^ final_hash >> 0x20;
 }
 
 uint64_t lua_bind_hash_str(char* str) {
@@ -85,5 +98,8 @@ uint64_t lua_bind_hash_str(char* str) {
 
 int main() {
     printf("Hash of 'password' = %" PRIx64 "\n", lua_bind_hash_str("password"));
+    printf("Hash of 'password1234' = %" PRIx64 "\n", lua_bind_hash_str("password1234"));
+    printf("Hash of 'FIGHTER_STATUS_DAMAGE_WORK_FLOAT_VECOR_CORRECT_STICK_X' = %" PRIx64 "\n", lua_bind_hash_str("FIGHTER_STATUS_DAMAGE_WORK_FLOAT_VECOR_CORRECT_STICK_X"));
+
     return 0;
 }

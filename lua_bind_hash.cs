eac2c1a49bb632b5ac878c8f98b07f73b02a1e25
jam1garner <jam1.mcleod@hotmail.com>
2019-05-16 13:52:22 -0400

    Rewrite C# impl, now fully works

--- lua_bind_hash.cs
+++ lua_bind_hash.cs
@@ -3,84 +3,92 @@ using System.Text;
 
 class LuaBindHash {
     unsafe static ulong Hash(byte[] data_) {
-        long *data;
+        ulong *data;
+        ulong len = (ulong)data_.Length;
         fixed (byte *data__ = data_)
         {
-            data = (long*)data__;
+            data = (ulong*)data__;
         }
-        ulong len = (ulong)data_.Length;
-        long *plVar1;
-        long *plVar2;
-        ulong uVar3;
-        ulong uVar4;
-        long lVar5;
-        ulong uVar6;
-        ulong uVar7;
-        ulong uVar8;
-        ulong uVar9;
-        ulong uVar10;
-        ulong uVar11;
+        ulong hash = len;
+        ulong hash_add = 0x27d4eb2f165667c4;
+        ulong* data_end = (ulong *)((ulong)data + len);
+
+        ulong[] hash_vals = new ulong[] {
+           0x60ea27eeadc0b5d5,
+           0xc2b2ae3d27d4eb4e,
+           0xffffffffffffffff,
+           0x61c8864e7a143578,
+        };
+        ulong[] hash_vals_mid = new ulong[4];
+        ulong[] hash_vals_end = new ulong[] {
+            0x3c6ef3630bd7950e,
+            0x1bbcd8c2f5e54380,
+            0x779b185ebca87000,
+            0xe6c617af2a1c0000
+        };
+        ulong[] hash_vals_end_2 = new ulong[] {
+            0x3f,
+            0x39,
+            0x34,
+            0x2e
+        };
+
+        const ulong MULT1 = 0x87bcb65480000000; // -0x784349ab80000000
+        const ulong MULT2 = 0xdef35b010f796ca9; // -0x210ca4fef0869357
+        const ulong MULT3 = 0x9e3779b185ebca87; // -0x61c8864e7a143579
+        const ulong MULT4 = 0xc2b2ae3d27d4eb4f; // -0x3d4d51c2d82b14b1
+        const ulong MULT5 = 0x93ea75a780000000; // -0x6c158a5880000000
+        const ulong MULT6 = 0x27d4eb2f165667c5;
+        const ulong ADD1 = 0x85ebca77c2b2ae63;
+        const ulong ADD2 = 0x165667b19e3779f9;
         
-        lVar5 = 0x27d4eb2f165667c4;
-        plVar1 = (long *)((ulong)data + len);
-        if (0x1f < len) {
-            uVar9 = 0xc2b2ae3d27d4eb4e;
-            uVar6 = 0x61c8864e7a143578;
-            uVar7 = 0xffffffffffffffff;
-            uVar3 = 0x60ea27eeadc0b5d5;
+        if (len >= 32) {
             do {
-                uVar3 = (ulong)((long)uVar3 + *data * -0x3d4d51c2d82b14b1);
-                uVar4 = uVar3 >> 0x21 | uVar3 * 0x80000000;
-                uVar9 += (ulong)(data[1] * -0x3d4d51c2d82b14b1);
-                uVar7 += (ulong)(data[2] * -0x3d4d51c2d82b14b1);
-                uVar6 += (ulong)(data[3] * -0x3d4d51c2d82b14b1);
-                uVar10 = uVar9 >> 0x21 | uVar9 * 0x80000000;
-                uVar9 = (ulong)((long)uVar10 * -0x61c8864e7a143579);
-                uVar11 = uVar7 >> 0x21 | uVar7 * 0x80000000;
-                uVar8 = uVar6 >> 0x21 | uVar6 * 0x80000000;
-                uVar3 = (ulong)((long)uVar4 * -0x61c8864e7a143579);
-                uVar7 = (ulong)((long)uVar11 * -0x61c8864e7a143579);
-                uVar6 = (ulong)((long)uVar8 * -0x61c8864e7a143579);
+                for (int i = 0; i < 4; i++) {
+                    hash_vals[i] += data[i] * MULT4;
+                    hash_vals_mid[i] = hash_vals[i] >> 0x21 | hash_vals[i] * 0x80000000;
+                    hash_vals[i] = hash_vals_mid[i] * MULT3;
+                }
+              
                 data = data + 4;
-            } while (data <= plVar1 + -4);
-            lVar5 = (long)((((((uVar10 * 0x1bbcd8c2f5e54380 | uVar9 >> 0x39) +
-                        (uVar4 * 0x3c6ef3630bd7950e | uVar3 >> 0x3f) +
-                        (uVar11 * 0x779b185ebca87000 | uVar7 >> 0x34) +
-                        (uVar8 * 0xe6c617af2a1c0000 | uVar6 >> 0x2e) ^
-                        (uVar4 * 0x87bcb65480000000 | uVar4 * 0xdef35b010f796ca9 >> 0x21) *
-                        0x9e3779b185ebca87) * 0x9e3779b185ebca87 + 0x85ebca77c2b2ae63 ^
-                        (uVar10 * 0x87bcb65480000000 | uVar10 * 0xdef35b010f796ca9 >> 0x21) *
-                        0x9e3779b185ebca87) * 0x9e3779b185ebca87 + 0x85ebca77c2b2ae63 ^
-                        (uVar11 * 0x87bcb65480000000 | uVar11 * 0xdef35b010f796ca9 >> 0x21) *
-                        0x9e3779b185ebca87) * 0x9e3779b185ebca87 + 0x85ebca77c2b2ae63 ^
-                        (uVar8 * 0x87bcb65480000000 | uVar8 * 0xdef35b010f796ca9 >> 0x21) *
-                        0x9e3779b185ebca87) * 0x9e3779b185ebca87 + 0x85ebca77c2b2ae63);
+            } while (data <= data_end + -4);
+            ulong val = 0;
+            for (int i = 0; i < 4; i++) {
+                val += (hash_vals_mid[i] * hash_vals_end[i] | hash_vals[i] >> (int)hash_vals_end_2[i]);
+            }
+            
+            val = (val ^ (hash_vals_mid[0] * MULT1 | hash_vals_mid[0] * MULT2 >> 0x21) * MULT3) * MULT3;
+            for (int i = 1; i < 4; i++) {
+                val = (val + ADD1 ^ (hash_vals_mid[i] * MULT1 | hash_vals_mid[i] * MULT2 >> 0x21) * MULT3) * MULT3;
+            }
+            val += ADD1;
+            hash_add = val;
         }
-        plVar2 = data + 1;
-        len = (ulong)lVar5 + len;
-        while (plVar2 <= plVar1) {
-            plVar2 = data + 2;
-            len = (ulong)((long)((ulong)((long)*data * -0x6c158a5880000000) | (ulong)((long)*data * -0x3d4d51c2d82b14b1) >> 0x21) *
-                        -0x61c8864e7a143579) ^ len;
-            len = (ulong)((long)(len >> 0x25 | len << 0x1b) * -0x61c8864e7a143579) + 0x85ebca77c2b2ae63;
-            data = data + 1;
+
+        hash += hash_add;
+        for (; data + 1 <= data_end; data++) {
+            hash = (*data * MULT5 | (*data * MULT4) >> 0x21) *
+                        MULT3 ^ hash;
+            hash = (hash >> 0x25 | hash << 0x1b) * MULT3 + ADD1;
         }
-        if ((long *)((long)data + 4U) <= plVar1) {
-            len = (ulong)((long)((ulong)*(ulong*)data) * -0x61c8864e7a143579) ^ len;
-            len = (ulong)((long)(len >> 0x29 | len << 0x17) * -0x3d4d51c2d82b14b1) + 0x165667b19e3779f9;
-            data = (long *)((long)data + 4U);
+
+        uint* data_32 = (uint*) data; 
+        if ((ulong *)(data_32 + 1) <= data_end) {
+            hash = (uint)*data_32 * MULT3 ^ hash;
+            hash = (hash >> 0x29 | hash << 0x17) * MULT4 + ADD2;
+            data = (ulong *)(data_32 + 1);
         }
-        if (data < plVar1) {
-            do {
-                plVar2 = (long *)((long)data + 1);
-                len = (ulong)*(byte *)data * 0x27d4eb2f165667c5 ^ len;
-                len = (ulong)((long)(len >> 0x35 | len << 0xb) * -0x61c8864e7a143579);
-                data = plVar2;
-            } while (plVar1 != plVar2);
+
+        byte* data_8 = (byte*) data;
+        for (; data != data_end; data = (ulong*)data_8) {
+            hash = (ulong)(*data_8) * MULT6 ^ hash;
+            hash = (hash >> 0x35 | hash << 0xb) * MULT3;
+            data_8++;
         }
-        uVar3 = (ulong)((long)(len ^ len >> 0x21) * -0x3d4d51c2d82b14b1);
-        uVar3 = (uVar3 ^ uVar3 >> 0x1d) * 0x165667b19e3779f9;
-        return uVar3 ^ uVar3 >> 0x20;
+        
+        ulong final_hash = (hash ^ hash >> 0x21) * MULT4;
+        final_hash = (final_hash ^ final_hash >> 0x1d) * ADD2;
+        return final_hash ^ final_hash >> 0x20;
     }
 
     static ulong HashString(string s) {
@@ -89,7 +97,14 @@ class LuaBindHash {
 
     static void Main() {
         // Should print A4D50A730E36970E
-        Console.WriteLine(HashString("password1234").ToString("X"));
+        string[] tests = new string[] {
+            "password",
+            "password1234",
+            "FIGHTER_STATUS_DAMAGE_WORK_FLOAT_VECOR_CORRECT_STICK_X"
+        };
+        foreach(var test in tests) {
+            Console.WriteLine($"Hash of '{test}' = {HashString(test):x}");
+        }
     }
 }
 
